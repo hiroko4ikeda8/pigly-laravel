a1cddedb1c100ef8847765960a41ef7de7f47766
hiroko ikeda <art_stylist_spinning@yahoo.co.jp>
2025-01-29 21:45:49 +0900

    Update User model and add WeightLog model

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -41,4 +41,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // ユーザーとのリレーション
+    public function weightLogs()
+    {
+        return $this->hasMany(WeightLog::class);
+    }
 }
